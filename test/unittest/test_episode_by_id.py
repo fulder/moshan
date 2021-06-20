585328f7c5fd9a79db447c41662ff61af96ac7fd
fulder <misad90@gmail.com>
2021-06-20 17:32:34 +0200

    Refactor episode by id tests to classes per method

--- test/unittest/test_episode_by_id.py
+++ test/unittest/test_episode_by_id.py
@@ -1,3 +1,4 @@
+import copy
 from unittest.mock import patch
 
 from api.episode_by_id import handle
@@ -6,10 +7,7 @@ from episodes_db import NotFoundError
 TEST_JWT = "eyJraWQiOiIxMjMxMjMxMjM9IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VybmFtZSI6IlRFU1RfQ0xJRU5UX0lEIn0.ud_dRdguJwmKv4XO-c4JD-dKGffSvXsxuAxZq9uWV-g"
 
 
-@patch("api.episode_by_id.episodes_db.get_episode")
-def test_handler_get(mocked_get):
-    mocked_get.return_value = {"collection_name": "anime", "item_id": 123}
-
+class TestGet:
     event = {
         "headers": {
             "authorization": TEST_JWT
@@ -26,38 +24,31 @@ def test_handler_get(mocked_get):
         }
     }
 
-    ret = handle(event, None)
-    assert ret == {'body': '{"collection_name": "anime", "item_id": 123}', 'statusCode': 200}
+    @patch("api.episode_by_id.episodes_db.get_episode")
+    def test_success(self, mocked_get):
+        mocked_get.return_value = {"collection_name": "anime", "item_id": 123}
+
+        ret = handle(self.event, None)
+        assert ret == {'body': '{"collection_name": "anime", "item_id": 123}',
+                       'statusCode': 200}
+
+    @patch("api.episode_by_id.episodes_db.get_episode")
+    def test_not_found(self, mocked_get):
+        mocked_get.side_effect = NotFoundError
+
+        ret = handle(self.event, None)
+        assert ret == {'statusCode': 404}
+
+    def test_invalid_collection_name(self):
+        event = copy.deepcopy(self.event)
+        event["pathParameters"]["collection_name"] = "INVALID",
+
+        ret = handle(event, None)
+        assert ret == {'statusCode': 400,
+                       'body': '{"message": "Invalid collection name, allowed values: [\'anime\', \'show\', \'movie\']"}'}
 
 
-@patch("api.episode_by_id.episodes_db.get_episode")
-def test_handler_get_not_found(mocked_get):
-    mocked_get.side_effect = NotFoundError
-
-    event = {
-        "headers": {
-            "authorization": TEST_JWT
-        },
-        "requestContext": {
-            "http": {
-                "method": "GET"
-            }
-        },
-        "pathParameters": {
-            "collection_name": "anime",
-            "item_id": "123",
-            "episode_id": "345"
-        }
-    }
-
-    ret = handle(event, None)
-    assert ret == {'statusCode': 404}
-
-
-@patch("api.episode_by_id.episodes_db.delete_episode")
-def test_handler_delete(mocked_delete):
-    mocked_delete.return_value = True
-
+class TestDelete:
     event = {
         "headers": {
             "authorization": TEST_JWT
@@ -74,18 +65,15 @@ def test_handler_delete(mocked_delete):
         }
     }
 
-    ret = handle(event, None)
-    assert ret == {'statusCode': 204}
+    @patch("api.episode_by_id.episodes_db.delete_episode")
+    def test_success(self, mocked_delete):
+        mocked_delete.return_value = True
+
+        ret = handle(self.event, None)
+        assert ret == {'statusCode': 204}
 
 
-@patch("api.episode_by_id.episodes_db.update_episode")
-@patch("api.episode_by_collection_item.anime_api.post_episode")
-def test_handler_patch(mocked_post_episode, mocked_post):
-    mocked_post.return_value = True
-    mocked_post_episode.return_value.json.return_value = {
-        "id": "123"
-    }
-
+class TestPatch:
     event = {
         "headers": {
             "authorization": TEST_JWT
@@ -103,101 +91,42 @@ def test_handler_patch(mocked_post_episode, mocked_post):
         "body": '{"rating": 3, "overview": "My overview", "review": "My review"}'
     }
 
-    ret = handle(event, None)
-    assert ret == {'statusCode': 204}
-
-
-@patch("api.episode_by_id.episodes_db.update_episode")
-def test_handler_patch_validation_failure(mocked_post):
-    mocked_post.return_value = True
-
-    event = {
-        "headers": {
-            "authorization": TEST_JWT
-        },
-        "requestContext": {
-            "http": {
-                "method": "PATCH"
-            }
-        },
-        "pathParameters": {
-            "collection_name": "anime",
-            "item_id": "123",
-            "episode_id": "345"
-        },
-        "body": '{"rating": "ABC"}'
-    }
-
-    ret = handle(event, None)
-    assert ret == {'statusCode': 400, 'body': '{"message": "Invalid post schema", "error": "\'ABC\' is not of type \'integer\'"}'}
-
-
-@patch("api.episode_by_id.episodes_db.update_episode")
-def test_handler_patch_block_additional_properties(mocked_post):
-    mocked_post.return_value = True
-
-    event = {
-        "headers": {
-            "authorization": TEST_JWT
-        },
-        "requestContext": {
-            "http": {
-                "method": "PATCH"
-            }
-        },
-        "pathParameters": {
-            "collection_name": "anime",
-            "item_id": "123",
-            "episode_id": "345"
-        },
-        "body": '{"rating": 1, "werid_property": "123"}'
-    }
-
-    ret = handle(event, None)
-    assert ret == {'statusCode': 400, 'body': '{"message": "Invalid post schema", "error": "Additional properties are not allowed (\'werid_property\' was unexpected)"}'}
-
-
-@patch("api.episode_by_id.episodes_db.update_episode")
-def test_handler_patch_invalid_body_format(mocked_post):
-    mocked_post.return_value = True
-
-    event = {
-        "headers": {
-            "authorization": TEST_JWT
-        },
-        "requestContext": {
-            "http": {
-                "method": "PATCH"
-            }
-        },
-        "pathParameters": {
-            "collection_name": "anime",
-            "item_id": "123",
-            "episode_id": "345"
-        },
-        "body": 'INVALID'
-    }
-
-    ret = handle(event, None)
-    assert ret == {'body': 'Invalid patch body', 'statusCode': 400}
-
-
-def test_handler_invalid_collection_name():
-    event = {
-        "headers": {
-            "authorization": TEST_JWT
-        },
-        "requestContext": {
-            "http": {
-                "method": "GET"
-            }
-        },
-        "pathParameters": {
-            "collection_name": "INVALID",
-            "item_id": "123",
-            "episode_id": "345"
+    @patch("api.episode_by_id.episodes_db.update_episode")
+    @patch("api.episode_by_collection_item.anime_api.post_episode")
+    def test_success(self, mocked_post_episode, mocked_post):
+        mocked_post.return_value = True
+        mocked_post_episode.return_value.json.return_value = {
+            "id": "123"
         }
-    }
 
-    ret = handle(event, None)
-    assert ret == {'statusCode': 400, 'body': '{"message": "Invalid collection name, allowed values: [\'anime\', \'show\', \'movie\']"}'}
+        ret = handle(self.event, None)
+        assert ret == {'statusCode': 204}
+
+    @patch("api.episode_by_id.episodes_db.update_episode")
+    def test_invalid_body_type(self, mocked_post):
+        mocked_post.return_value = True
+        event = copy.deepcopy(self.event)
+        event["body"] = '{"rating": "ABC"}'
+
+        ret = handle(event, None)
+        assert ret == {'statusCode': 400,
+                       'body': '{"message": "Invalid post schema", "error": "\'ABC\' is not of type \'integer\'"}'}
+
+    @patch("api.episode_by_id.episodes_db.update_episode")
+    def test_block_additional_properties(self, mocked_post):
+        mocked_post.return_value = True
+        event = copy.deepcopy(self.event)
+        event["body"] = '{"rating": 1, "werid_property": "123"}'
+
+        ret = handle(event, None)
+        assert ret == {'statusCode': 400,
+                       'body': '{"message": "Invalid post schema", "error": "Additional properties are not allowed (\'werid_property\' was unexpected)"}'}
+
+    @patch("api.episode_by_id.episodes_db.update_episode")
+    def test_invalid_body_format(self, mocked_post):
+        mocked_post.return_value = True
+        event = copy.deepcopy(self.event)
+        event["body"] = "INVALID"
+
+        ret = handle(event, None)
+        assert ret == {'body': 'Invalid patch body', 'statusCode': 400}
