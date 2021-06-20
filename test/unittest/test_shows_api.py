ba247cda27a61cdad8b6a3d84f87e1fb0ff091de
fulder <misad90@gmail.com>
2021-06-20 22:41:46 +0200

    Add missing post anime and shows episodes tests

--- test/unittest/test_shows_api.py
+++ test/unittest/test_shows_api.py
@@ -34,7 +34,8 @@ def test_post_show(mocked_post, mocked_show_api):
     m.json.return_value = {"show_id": "123"}
     mocked_post.return_value = m
 
-    ret = mocked_show_api.post_show({"api_id": "123", "api_name": "tvmaze"}, "TEST_TOKEN")
+    ret = mocked_show_api.post_show({"api_id": "123", "api_name": "tvmaze"},
+                                    "TEST_TOKEN")
 
     assert ret == {"show_id": "123"}
 
@@ -46,4 +47,35 @@ def test_post_show_invalid_code(mocked_post, mocked_show_api):
     mocked_post.return_value = m
 
     with pytest.raises(api_errors.HttpError):
-        mocked_show_api.post_show({"api_id": "123", "api_name": "tvmaze"}, "TEST_TOKEN")
+        mocked_show_api.post_show({"api_id": "123", "api_name": "tvmaze"},
+                                  "TEST_TOKEN")
+
+
+@patch("shows_api.requests.post")
+def test_post_episode(mocked_post, mocked_show_api):
+    m = MagicMock()
+    m.status_code = 200
+    m.json.return_value = {"show_id": "123"}
+    mocked_post.return_value = m
+
+    ret = mocked_show_api.post_episode(
+        "123",
+        {"api_id": "123", "api_name": "tvmaze"},
+        "TEST_TOKEN"
+    )
+
+    assert ret == {"show_id": "123"}
+
+
+@patch("shows_api.requests.post")
+def test_post_episode_invalid_code(mocked_post, mocked_show_api):
+    m = MagicMock()
+    m.status_code = 404
+    mocked_post.return_value = m
+
+    with pytest.raises(api_errors.HttpError):
+        mocked_show_api.post_episode(
+            "123",
+            {"api_id": "123", "api_name": "tvmaze"},
+            "TEST_TOKEN"
+        )
