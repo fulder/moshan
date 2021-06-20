22bb7b19b655c4f81ed039cc22f091d2d10ebaa9
fulder <misad90@gmail.com>
2021-06-20 22:41:46 +0200

    Add missing post anime and shows episodes tests

--- test/unittest/test_anime_api.py
+++ test/unittest/test_anime_api.py
@@ -47,3 +47,25 @@ def test_post_anime_invalid_code(mocked_post, mocked_anime_api):
 
     with pytest.raises(api_errors.HttpError):
         mocked_anime_api.post_anime({}, "TEST_TOKEN")
+
+
+@patch("anime_api.requests.post")
+def test_post_episode(mocked_post, mocked_anime_api):
+    m = MagicMock()
+    m.status_code = 200
+    m.json.return_value = {"anime_id": "123"}
+    mocked_post.return_value = m
+
+    ret = mocked_anime_api.post_episode("123", {}, "TEST_TOKEN")
+
+    assert ret == {"anime_id": "123"}
+
+
+@patch("anime_api.requests.post")
+def test_post_episode_invalid_code(mocked_post, mocked_anime_api):
+    m = MagicMock()
+    m.status_code = 404
+    mocked_post.return_value = m
+
+    with pytest.raises(api_errors.HttpError):
+        mocked_anime_api.post_episode("123", {}, "TEST_TOKEN")
