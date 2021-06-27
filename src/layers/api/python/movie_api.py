757c381a1dce5446fe6f79159bf2ff65a66ce01e
fulder <misad90@gmail.com>
2021-06-27 14:18:24 +0200

    Allow getting item by third party api_id

--- src/layers/api/python/movie_api.py
+++ src/layers/api/python/movie_api.py
@@ -14,6 +14,14 @@ def get_movie(movie_id, token):
     return res.json()
 
 
+def get_movie_by_api_id(api_name, api_id, token):
+    res = requests.get(f"{MOVIE_API_URL}/movies?api_name={api_name}&api_id={api_id}", headers={"Authorization": token})
+    if res.status_code != 200:
+        raise api_errors.HttpError("Invalid response in get_shows_by_api_id", res.status_code)
+
+    return res.json()
+
+
 def post_movie(body, token):
     res = requests.post(f"{MOVIE_API_URL}/movies", headers={"Authorization": token}, json=body)
     if res.status_code != 200:
