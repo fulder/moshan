4785e0976b7c7fd24b41320c56b8104c33424813
fulder <misad90@gmail.com>
2021-06-27 23:55:51 +0200

    Add shows and anime functions to get episodes by api_id

--- src/layers/api/python/shows_api.py
+++ src/layers/api/python/shows_api.py
@@ -30,6 +30,14 @@ def post_show(body, token):
     return res.json()
 
 
+def get_episode_by_api_id(api_name, api_id, token):
+    res = requests.get(f"{SHOWS_API_URL}/episodes?api_name={api_name}&api_id={api_id}", headers={"Authorization": token})
+    if res.status_code != 200:
+        raise api_errors.HttpError("Invalid response in get_shows_by_api_id", res.status_code)
+
+    return res.json()
+
+
 def post_episode(item_id, body, token):
     res = requests.post(f"{SHOWS_API_URL}/shows/{item_id}/episodes", headers={"Authorization": token}, json=body)
     if res.status_code != 200:
