2d6794fb4382d3ea148b6ee8d54554e615b5536e
fulder <misad90@gmail.com>
2021-07-21 00:23:08 +0200

    Revert "Change page during limit change only if page is not 1"
    This reverts commit 68f3768ecae42c4f254b32c84ae78c93df7dba89.

--- includes/js/watch_history.js
+++ includes/js/watch_history.js
@@ -190,7 +190,7 @@ async function loadItems(page, limit, collectionName, button=undefined) {
     document.getElementById(divName).getElementsByTagName('LI')[qParams[pageParamName]].classList.remove('active');
     qParams[pageParamName] = page;
   }
-  if (limitChanged && page != 1) {
+  if (limitChanged) {
     page = page / (limit / qParams[limitParamName] );
     qParams[pageParamName] = page;
   }
