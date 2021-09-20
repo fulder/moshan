cb7c3cbba8d66ab8eafd0beef31ced09c98107ad
fulder <misad90@gmail.com>
2021-09-20 23:24:47 +0200

    Add first very simple backlog page script

---
+++ includes/js/backlog.js
@@ -0,0 +1,22 @@
+/* global WatchHistoryApi, accessToken, collectionNames */
+//const urlParams = new URLSearchParams(window.location.search);
+
+const watchHistoryApi = new WatchHistoryApi();
+
+if (accessToken === null) {
+  document.getElementById('logInAlert').className = 'alert alert-danger';
+} else {
+  document.getElementById('logInAlert').className = 'd-none';
+}
+
+createTableRows();
+
+async function createTableRows() {
+    const response = await watchHistoryApi.getWatchHistory('backlog');
+    const items = response.data.items;
+
+    for (let i=0; i < items.length; i++) {
+      console.debug(items[i]);
+    }
+
+}
