bb5824fceae1d309b632cc075dfeca432809d45e
fulder <misad90@gmail.com>
2021-10-02 16:55:18 +0200

    Add v4 signature to shows api requests

--- src/layers/api/python/shows_api.py
+++ src/layers/api/python/shows_api.py
@@ -2,12 +2,13 @@ import os
 import requests
 
 import api_errors
+from utils import get_v4_signature_auth
 
 SHOWS_API_URL = os.getenv("SHOWS_API_URL")
 
 
 def get_show(show_id):
-    res = requests.get(f"{SHOWS_API_URL}/shows/{show_id}")
+    res = requests.get(f"{SHOWS_API_URL}/shows/{show_id}", auth=get_v4_signature_auth())
     if res.status_code != 200:
         raise api_errors.HttpError("Invalid response in get_show", res.status_code)
 
@@ -15,7 +16,7 @@ def get_show(show_id):
 
 
 def get_show_by_api_id(api_name, api_id):
-    res = requests.get(f"{SHOWS_API_URL}/shows?api_name={api_name}&api_id={api_id}")
+    res = requests.get(f"{SHOWS_API_URL}/shows?api_name={api_name}&api_id={api_id}", auth=get_v4_signature_auth())
     if res.status_code != 200:
         raise api_errors.HttpError("Invalid response in get_shows_by_api_id", res.status_code)
 
@@ -23,7 +24,7 @@ def get_show_by_api_id(api_name, api_id):
 
 
 def post_show(body):
-    res = requests.post(f"{SHOWS_API_URL}/shows", json=body)
+    res = requests.post(f"{SHOWS_API_URL}/shows", json=body, auth=get_v4_signature_auth())
     if res.status_code != 200:
         raise api_errors.HttpError("Invalid response during show post", res.status_code)
 
@@ -31,7 +32,7 @@ def post_show(body):
 
 
 def get_episode(item_id, episode_id):
-    res = requests.get(f"{SHOWS_API_URL}/shows/{item_id}/episodes/{episode_id}")
+    res = requests.get(f"{SHOWS_API_URL}/shows/{item_id}/episodes/{episode_id}", auth=get_v4_signature_auth())
     if res.status_code != 200:
         raise api_errors.HttpError("Invalid response during show post", res.status_code)
 
@@ -39,7 +40,7 @@ def get_episode(item_id, episode_id):
 
 
 def get_episode_by_api_id(api_name, api_id):
-    res = requests.get(f"{SHOWS_API_URL}/episodes?api_name={api_name}&api_id={api_id}")
+    res = requests.get(f"{SHOWS_API_URL}/episodes?api_name={api_name}&api_id={api_id}", auth=get_v4_signature_auth())
     if res.status_code != 200:
         raise api_errors.HttpError("Invalid response in get_shows_by_api_id", res.status_code)
 
@@ -47,7 +48,7 @@ def get_episode_by_api_id(api_name, api_id):
 
 
 def post_episode(item_id, body):
-    res = requests.post(f"{SHOWS_API_URL}/shows/{item_id}/episodes", json=body)
+    res = requests.post(f"{SHOWS_API_URL}/shows/{item_id}/episodes", json=body, auth=get_v4_signature_auth())
     if res.status_code != 200:
         raise api_errors.HttpError("Invalid response during show post", res.status_code)
 
