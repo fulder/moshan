ecd7219efbc5174480233d2eb54adc8345827ba7
fulder <misad90@gmail.com>
2021-10-02 18:39:25 +0200

    Recalculate all new item properties after item update

--- src/lambdas/subscribers/show_updates/__init__.py
+++ src/lambdas/subscribers/show_updates/__init__.py
@@ -17,11 +17,13 @@ def handler(event, context):
         if "ep_progress" not in item["ep_progress"]:
             item["ep_progress"] = 0
             item["watched_eps"] = 0
+            item["special_progress"] = 0
+            item["watched_special"] = 0
 
+        item["ep_count"] = show["ep_count"]
+        ep_progress = item["watched_eps"] / item["ep_count"]
+        item["ep_progress"] = str(round(ep_progress * 100, 2))
 
-            item["specials_progress"] = 0
-            item["watched_specials"] = 0
-
-
-
-
+        item["special_count"] = show["special_count"]
+        special_progress = item["special_eps"] / item["special_count"]
+        item["special_progress"] = str(round(special_progress * 100, 2))
