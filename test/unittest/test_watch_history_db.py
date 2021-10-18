e54b2aa77fc702f4efb3d8dc4bb0e21d48e68510
fulder <misad90@gmail.com>
2021-10-18 23:18:44 +0200

    Fix failing tests

--- test/unittest/test_watch_history_db.py
+++ test/unittest/test_watch_history_db.py
@@ -4,52 +4,59 @@ from unittest.mock import MagicMock
 
 import pytest
 
-UPDATE_VALUES = {}
-MOCK_RETURN = []
 TEST_USERNAME = "TEST_USERNAME"
 
 
-def mock_func(**kwargs):
-    global UPDATE_VALUES
-    UPDATE_VALUES = kwargs
+class MockFunc:
+    def __init__(self, mock_return=[]):
+        self.mock_return = mock_return
+        self.update_values = {}
 
-    return MOCK_RETURN
+    def f(self, **kwargs):
+        self.update_values = kwargs
+        return self.mock_return
+
+    @property
+    def exp_res(self):
+        exp_res = []
+        for r in self.mock_return:
+            exp_res += r["Items"]
+        return exp_res
 
 
 def test_get_watch_history(mocked_watch_history_db):
-    global MOCK_RETURN
-    MOCK_RETURN = [
+    mock_func = MockFunc([
         {"Items": [{"collection_name": "ANIME", "item_id": 123}]},
         {"Items": [{"collection_name": "MOVIE", "item_id": 123}]}
-    ]
+    ])
     m = MagicMock()
     mocked_watch_history_db.client.get_paginator.return_value = m
-    m.paginate = mock_func
+    m.paginate = mock_func.f
 
-    mocked_watch_history_db.get_watch_history(TEST_USERNAME)
+    res = mocked_watch_history_db.get_watch_history(TEST_USERNAME)
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         "TableName": None,
         "KeyConditionExpression": "username = :username",
         "ExpressionAttributeValues": {":username": {"S": "TEST_USERNAME"}},
         "ScanIndexForward": False,
         'FilterExpression': 'attribute_not_exists(deleted_at)',
     }
+    assert res == mock_func.exp_res
 
 
 def test_get_watch_history_by_collection_name(mocked_watch_history_db):
-    global MOCK_RETURN
-    MOCK_RETURN = [
+    mock_func = MockFunc([
         {"Items": [{"collection_name": "ANIME", "item_id": 123}]}
-    ]
+    ])
     m = MagicMock()
     mocked_watch_history_db.client.get_paginator.return_value = m
-    m.paginate = mock_func
+    m.paginate = mock_func.f
 
-    mocked_watch_history_db.get_watch_history(TEST_USERNAME,
-                                              collection_name="anime")
+    res = mocked_watch_history_db.get_watch_history(TEST_USERNAME,
+                                                    collection_name="anime")
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         "ExpressionAttributeValues": {
             ":username": {"S": "TEST_USERNAME"},
             ":collection_name": {"S": "anime"}
@@ -58,24 +65,23 @@ def test_get_watch_history_by_collection_name(mocked_watch_history_db):
         "KeyConditionExpression": "username = :username",
         "ScanIndexForward": False,
         "TableName": None,
-
     }
+    assert res == mock_func.exp_res
 
 
 def test_get_watch_history_by_collection_and_index(mocked_watch_history_db):
-    global MOCK_RETURN
-    MOCK_RETURN = [
+    mock_func = MockFunc([
         {"Items": [{"collection_name": "ANIME", "item_id": 123}]}
-    ]
+    ])
     m = MagicMock()
     mocked_watch_history_db.client.get_paginator.return_value = m
-    m.paginate = mock_func
+    m.paginate = mock_func.f
 
-    mocked_watch_history_db.get_watch_history(TEST_USERNAME,
+    res = mocked_watch_history_db.get_watch_history(TEST_USERNAME,
                                               collection_name="ANIME",
                                               index_name="test_index")
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         "ExpressionAttributeValues": {
             ":username": {"S": "TEST_USERNAME"},
             ":collection_name": {"S": "ANIME"}
@@ -86,17 +92,17 @@ def test_get_watch_history_by_collection_and_index(mocked_watch_history_db):
         "ScanIndexForward": False,
         "TableName": None
     }
+    assert res == mock_func.exp_res
 
 
 def test_add_item(mocked_watch_history_db):
-    global UPDATE_VALUES
-    UPDATE_VALUES = {}
-    mocked_watch_history_db.table.update_item = mock_func
+    mock_func = MockFunc()
+    mocked_watch_history_db.table.update_item = mock_func.f
     mocked_watch_history_db.table.query.side_effect = mocked_watch_history_db.NotFoundError
 
     mocked_watch_history_db.add_item(TEST_USERNAME, "MOVIE", "123123")
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         'ExpressionAttributeNames': {
             '#collection_name': 'collection_name',
             '#created_at': 'created_at',
@@ -119,16 +125,15 @@ def test_add_item(mocked_watch_history_db):
 
 
 def test_add_item_already_exists(mocked_watch_history_db):
-    global UPDATE_VALUES
-    UPDATE_VALUES = {}
-    mocked_watch_history_db.table.update_item = mock_func
+    mock_func = MockFunc()
+    mocked_watch_history_db.table.update_item = mock_func.f
     mocked_watch_history_db.table.query.return_value = {
         "Items": [{"exists"}]
     }
 
     mocked_watch_history_db.add_item(TEST_USERNAME, "MOVIE", "123123")
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         'ExpressionAttributeNames': {
             '#collection_name': 'collection_name',
             '#deleted_at': 'deleted_at',
@@ -150,14 +155,13 @@ def test_add_item_already_exists(mocked_watch_history_db):
 
 
 def test_update_item(mocked_watch_history_db):
-    global UPDATE_VALUES
-    UPDATE_VALUES = {}
-    mocked_watch_history_db.table.update_item = mock_func
+    mock_func = MockFunc()
+    mocked_watch_history_db.table.update_item = mock_func.f
 
     mocked_watch_history_db.update_item(TEST_USERNAME, "MOVIE", "123",
                                         {"review": "review_text"})
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         'ExpressionAttributeNames': {
             '#collection_name': 'collection_name',
             '#dates_watched': 'dates_watched',
@@ -183,16 +187,15 @@ def test_update_item(mocked_watch_history_db):
 
 
 def test_update_item_dates_watched(mocked_watch_history_db):
-    global UPDATE_VALUES
-    UPDATE_VALUES = {}
-    mocked_watch_history_db.table.update_item = mock_func
+    mock_func = MockFunc()
+    mocked_watch_history_db.table.update_item = mock_func.f
 
     mocked_watch_history_db.update_item(TEST_USERNAME, "MOVIE", "123",
                                         {"dates_watched": [
                                             "2020-12-20T15:30:09.909Z",
                                             "2021-12-20T15:30:09.909Z"]})
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         'ExpressionAttributeNames': {
             '#collection_name': 'collection_name',
             '#updated_at': 'updated_at',
@@ -221,14 +224,13 @@ def test_update_item_dates_watched(mocked_watch_history_db):
 
 
 def test_update_item_dates_watched_one_date(mocked_watch_history_db):
-    global UPDATE_VALUES
-    UPDATE_VALUES = {}
-    mocked_watch_history_db.table.update_item = mock_func
+    mock_func = MockFunc()
+    mocked_watch_history_db.table.update_item = mock_func.f
 
     mocked_watch_history_db.update_item(TEST_USERNAME, "MOVIE", "123", {
         "dates_watched": ["2020-12-20T15:30:09.909Z"]})
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         'ExpressionAttributeNames': {
             '#collection_name': 'collection_name',
             '#dates_watched': 'dates_watched',
@@ -256,13 +258,12 @@ def test_update_item_dates_watched_one_date(mocked_watch_history_db):
 
 
 def test_delete_item(mocked_watch_history_db):
-    global UPDATE_VALUES
-    UPDATE_VALUES = {}
-    mocked_watch_history_db.table.update_item = mock_func
+    mock_func = MockFunc()
+    mocked_watch_history_db.table.update_item = mock_func.f
 
     mocked_watch_history_db.delete_item(TEST_USERNAME, "MOVIE", "123123")
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         'ExpressionAttributeNames': {
             '#collection_name': 'collection_name',
             '#deleted_at': 'deleted_at',
@@ -282,16 +283,15 @@ def test_delete_item(mocked_watch_history_db):
 
 
 def test_add_item_exists(mocked_watch_history_db):
-    global UPDATE_VALUES
-    UPDATE_VALUES = {}
-    mocked_watch_history_db.table.update_item = mock_func
+    mock_func = MockFunc()
+    mocked_watch_history_db.table.update_item = mock_func.f
     mocked_watch_history_db.table.query.return_value = {
         "Items": [{"item_data"}]
     }
 
     mocked_watch_history_db.add_item(TEST_USERNAME, "MOVIE", "123123")
 
-    assert UPDATE_VALUES == {
+    assert mock_func.update_values == {
         'ExpressionAttributeNames': {
             '#collection_name': 'collection_name',
             '#deleted_at': 'deleted_at',
@@ -311,10 +311,11 @@ def test_add_item_exists(mocked_watch_history_db):
 
 
 def test_get_item(mocked_watch_history_db):
-    global MOCK_RETURN
-    MOCK_RETURN = {"Items": [{"collection_name": "ANIME", "item_id": 123}]}
+    mock_func = MockFunc(
+        {"Items": [{"collection_name": "ANIME", "item_id": 123}]},
+    )
 
-    mocked_watch_history_db.table.query = mock_func
+    mocked_watch_history_db.table.query = mock_func.f
 
     ret = mocked_watch_history_db.get_item(TEST_USERNAME, "MOVIE", "123123")
 
