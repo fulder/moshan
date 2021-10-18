b806caae6b46c1feacc90f408b93f9c10917c539
fulder <misad90@gmail.com>
2021-10-18 23:58:19 +0200

    Upgrade CDK version

--- deploy/lib/watch_history.py
+++ deploy/lib/watch_history.py
@@ -3,11 +3,10 @@ import shutil
 import subprocess
 
 from aws_cdk import core
-from aws_cdk.aws_apigateway import DomainName, SecurityPolicy
 from aws_cdk.aws_apigatewayv2 import HttpApi, CfnAuthorizer, HttpIntegration, \
     HttpIntegrationType, HttpMethod, \
-    PayloadFormatVersion, CfnRoute, CfnStage, HttpApiMapping, \
-    CorsPreflightOptions
+    PayloadFormatVersion, CfnRoute, \
+    CorsPreflightOptions, DomainMappingOptions, HttpStage, DomainName
 from aws_cdk.aws_certificatemanager import Certificate, ValidationMethod
 from aws_cdk.aws_dynamodb import Table, Attribute, AttributeType, BillingMode
 from aws_cdk.aws_iam import PolicyStatement, Role, ServicePrincipal, \
@@ -359,7 +358,6 @@ class WatchHistory(core.Stack):
             "domain",
             domain_name=self.domain_name,
             certificate=cert,
-            security_policy=SecurityPolicy.TLS_1_2
         )
 
         http_api = HttpApi(
@@ -442,22 +440,13 @@ class WatchHistory(core.Stack):
                 source_arn=f"arn:aws:execute-api:{self.region}:{self.account}:{http_api.http_api_id}/*"
             )
 
-        stage = CfnStage(
+        HttpStage(
             self,
             "live",
-            api_id=http_api.http_api_id,
+            http_api=http_api,
             auto_deploy=True,
-            default_route_settings=CfnStage.RouteSettingsProperty(
-                throttling_burst_limit=10,
-                throttling_rate_limit=5
-            ),
-            stage_name="live"
-        )
-
-        HttpApiMapping(
-            self,
-            "mapping",
-            api=http_api,
-            domain_name=domain_name,
-            stage=stage
+            stage_name="live",
+            domain_mapping=DomainMappingOptions(
+                domain_name=domain_name,
+            )
         )
