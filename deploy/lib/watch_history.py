92f6795ece6ad88bba24dfaad99f7969d9e7a139
fulder <misad90@gmail.com>
2021-10-29 19:36:06 +0200

    Change ANY to real verbs
    This will collide with the OPTIONS CORS settings otherwise

--- deploy/lib/watch_history.py
+++ deploy/lib/watch_history.py
@@ -454,7 +454,7 @@ class WatchHistory(core.Stack):
 
         routes = {
             "watch_histories": {
-                "method": ["ANY"],
+                "method": ["GET", "POST", "DELETE"],
                 "route": "/{proxy+}",
                 "target_lambda": self.lambdas["api-watch_histories"]
             },
