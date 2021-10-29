295218ec860ac500a9d4c6253844e3bf64d5fa6b
fulder <misad90@gmail.com>
2021-10-29 08:44:46 +0200

    Add middelware for getting username from JWT

--- src/lambdas/api/watch_histories/__init__.py
+++ src/lambdas/api/watch_histories/__init__.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from mangum import Mangum
+
+from . import api
+import jwt_utils
 
 app = FastAPI()
 
 
 @app.get("/watch-histories/item")
 def item(api_name: str, api_id: str):
-    return {"message": f"Got {api_name}_{api_id}"}
+    return api.get_item(api_name, api_id)
+
+
+@app.middleware("http")
+def parse_token(request: Request, call_next):
+    username = jwt_utils.get_username(request.headers.get("authorization"))
+    request.state.username = username
+    return call_next
 
 
 handle = Mangum(app, api_gateway_base_path="/prod/")
