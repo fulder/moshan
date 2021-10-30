3df3aebd610bc99537861c7a5b84e168b7a80243
fulder <misad90@gmail.com>
2021-10-30 11:28:05 +0200

    Remove Bearer from auth header

--- src/lambdas/api/watch_histories/__init__.py
+++ src/lambdas/api/watch_histories/__init__.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
 import api
@@ -7,19 +6,6 @@ import jwt_utils
 from models import Item
 
 app = FastAPI()
-
-origins = [
-    "https://moshan.tv",
-    "https://beta.moshan.tv",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["authorization", "content-type"],
-)
 
 
 @app.get("/watch-histories/item")
@@ -36,8 +22,7 @@ def add_item(request: Request, item: Item):
 @app.middleware("http")
 def parse_token(request: Request, call_next):
     auth_header = request.headers.get("authorization")
-    jwt_str = auth_header.split("Bearer ")[1]
-    request.state.username = jwt_utils.get_username(jwt_str)
+    request.state.username = jwt_utils.get_username(auth_header)
     return call_next(request)
 
 
