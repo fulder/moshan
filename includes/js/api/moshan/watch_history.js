b495e27e75a42d2aedadfad9dfef84724c9317ff
fulder <misad90@gmail.com>
2021-11-06 23:50:53 +0100

    Remove old episode apis for anime

--- includes/js/api/moshan/watch_history.js
+++ includes/js/api/moshan/watch_history.js
@@ -41,10 +41,6 @@ class WatchHistoryApi {
     return this.apiAxios.post('/watch-histories/items', data);
   }
 
-  getWatchHistoryItem (qParams) {
-    return this.apiAxios.get(`/watch-history/collection/${qParams.collection}/${qParams.id}`);
-  }
-
   getWatchHistoryItemByApiId (qParams) {
     return this.apiAxios.get(`/watch-histories/items/${qParams.api_name}/${qParams.api_id}`);
   }
@@ -70,43 +66,23 @@ class WatchHistoryApi {
   }
 
   addWatchHistoryEpisode (qParams) {
-    let data = {
-      api_id: qParams.api_id,
-      api_name: qParams.api_name,
+    data = {
+      episode_api_id: qParams.episode_api_id,
     };
-    if (qParams.collection == 'show') {
-          data = {
-            episode_api_id: qParams.episode_api_id,
-          };
-          return this.apiAxios.post(`/watch-histories/items/${qParams.api_name}/${qParams.item_api_id}/episodes`, data);
-    }
-    return this.apiAxios.post(`/watch-history/collection/${qParams.collection}/${qParams.id}/episode`, data);
+    return this.apiAxios.post(`/watch-histories/items/${qParams.api_name}/${qParams.item_api_id}/episodes`, data);
   }
 
   removeWatchHistoryEpisode (qParams) {
-    if (qParams.collection == 'show') {
-          return this.apiAxios.delete(`/watch-histories/items/${qParams.api_name}/${qParams.item_api_id}/episodes/${qParams.episode_api_id}`);
-    }
-    return this.apiAxios.delete(`/watch-history/collection/${qParams.collection}/${qParams.id}/episode/${qParams.episode_id}?api_name=${qParams.api_name}`);
-  }
-
-  getWatchHistoryEpisode (qParams) {
-    return this.apiAxios.get(`/watch-history/collection/${qParams.collection}/${qParams.id}/episode/${qParams.episode_id}`);
+    return this.apiAxios.delete(`/watch-histories/items/${qParams.api_name}/${qParams.item_api_id}/episodes/${qParams.episode_api_id}`);
   }
 
   getWatchHistoryEpisodeByApiId (qParams) {
-    if (qParams.collection == 'show') {
-          return this.apiAxios.get(`/watch-histories/items/${qParams.api_name}/${qParams.item_api_id}/episodes/${qParams.episode_api_id}`);
-    }
-    return this.apiAxios.get(`/watch-history/collection/${qParams.collection}/${qParams.id}/episode?api_name=${qParams.api_name}&api_id=${qParams.api_id}`);
+    return this.apiAxios.get(`/watch-histories/items/${qParams.api_name}/${qParams.item_api_id}/episodes/${qParams.episode_api_id}`);
   }
 
   updateWatchHistoryEpisode (qParams, watchDates = []) {
     const data = {};
     data.dates_watched = watchDates;
-    if (qParams.collection == 'show') {
-          return this.apiAxios.put(`/watch-histories/items/${qParams.api_name}/${qParams.item_api_id}/episodes/${qParams.episode_api_id}`, data);
-    }
-    return this.apiAxios.put(`/watch-history/collection/${qParams.collection}/${qParams.id}/episode/${qParams.episode_id}`, data);
+    return this.apiAxios.put(`/watch-histories/items/${qParams.api_name}/${qParams.item_api_id}/episodes/${qParams.episode_api_id}`, data);
   }
 }
