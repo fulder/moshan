284c149d851a4a4746c192e6d1db28a0cbce7cd6
fulder <misad90@gmail.com>
2021-11-06 13:32:17 +0100

    Remove tests using old lambdas and methods

--- test/unittest/test_watch_history_by_collection.py
+++ test/unittest/test_watch_history_by_collection.py
@@ -46,50 +46,6 @@ class TestGet:
             "statusCode": 200
         }
 
-    @patch("api.watch_history_by_collection.watch_history_db.get_item")
-    @patch("api.watch_history_by_collection.anime_api.get_anime_by_api_id")
-    def test_success_by_api_id_anime(self, mocked_get_anime,
-                                     mocked_get_watch_history):
-        w_ret = {
-            "collection_name": "anime",
-            "item_id": 123,
-            "username": "user",
-        }
-        s_ret = {
-            "id": 123
-        }
-        mocked_get_anime.return_value = s_ret
-        mocked_get_watch_history.return_value = w_ret
-        event = copy.deepcopy(self.event)
-        event["queryStringParameters"] = {
-            "api_id": 123,
-            "api_name": "mal"
-        }
-
-        ret = handle(event, None)
-        exp_ret = {**w_ret, **s_ret}
-        assert ret == {
-            "body": json.dumps(exp_ret),
-            "statusCode": 200
-        }
-
-    @patch("api.watch_history_by_collection.anime_api.get_anime_by_api_id")
-    def test_get_by_api_id_http_error(self, mocked_get_anime):
-        mocked_get_anime.side_effect = HttpError("test-error", 503)
-        event = copy.deepcopy(self.event)
-        event["queryStringParameters"] = {
-            "api_id": 123,
-            "api_name": "mal"
-        }
-
-        ret = handle(event, None)
-
-        assert ret == {
-            "body": '{"message": "Could not get anime"}',
-            "error": "test-error",
-            "statusCode": 503
-        }
-
     def test_invalid_sort(self):
         event = copy.deepcopy(self.event)
         event["queryStringParameters"] = {
@@ -137,27 +93,6 @@ class TestGet:
             "body": json.dumps({"items": []})
         }
 
-    @patch("api.watch_history_by_collection.watch_history_db.get_item")
-    @patch("api.watch_history_by_collection.anime_api.get_anime_by_api_id")
-    def test_by_api_id_not_found(self, mocked_get_anime,
-                                 mocked_get_watch_history):
-        mocked_get_anime.return_value = {
-            "id": 123
-        }
-        mocked_get_watch_history.side_effect = NotFoundError
-
-        event = copy.deepcopy(self.event)
-        event["queryStringParameters"] = {
-            "api_id": 123,
-            "api_name": "mal"
-        }
-
-        ret = handle(event, None)
-
-        assert ret == {
-            "statusCode": 404
-        }
-
 
 class TestPost:
     event = {
@@ -175,56 +110,6 @@ class TestPost:
         "body": '{ "api_id": "123", "api_name": "mal" }'
     }
 
-    @patch("api.watch_history_by_collection.watch_history_db.add_item")
-    @patch("api.watch_history_by_collection.anime_api.post_anime")
-    @patch("api.watch_history_by_collection.watch_history_db.get_item")
-    def test_success(self, mocked_get_item, mocked_post_anime, mocked_post):
-        mocked_post_anime.return_value = {
-            "id": "123"
-        }
-        mocked_post.return_value = True
-
-        ret = handle(self.event, None)
-        assert ret == {
-            "statusCode": 200,
-            "body": '{"id": "123"}',
-        }
-
-    @patch("api.watch_history_by_collection.watch_history_db.update_item")
-    def test_missing_body(self, mocked_post):
-        mocked_post.return_value = True
-        event = copy.deepcopy(self.event)
-        del event["body"]
-
-        ret = handle(event, None)
-
-        assert ret == {"body": "Invalid post body", "statusCode": 400}
-
-    @patch("api.watch_history_by_collection.watch_history_db.update_item")
-    def test_empty_body(self, mocked_post):
-        mocked_post.return_value = True
-        event = copy.deepcopy(self.event)
-        event["body"] = {}
-
-        ret = handle(event, None)
-        assert ret == {
-            "body": "Invalid post body",
-            "statusCode": 400
-        }
-
-    @patch("api.watch_history_by_collection.watch_history_db.update_item")
-    @patch("api.watch_history_by_collection.anime_api.post_anime")
-    def test_anime_http_error(self, mocked_get_anime, mocked_post):
-        mocked_get_anime.side_effect = HttpError("test_error", 403)
-        mocked_post.return_value = True
-
-        ret = handle(self.event, None)
-        assert ret == {
-            "body": '{"message": "Could not post anime"}',
-            "error": "test_error",
-            "statusCode": 403
-        }
-
     @patch("api.watch_history_by_collection.watch_history_db.update_item")
     def test_invalid_collection(self, mocked_post):
         mocked_post.return_value = True
@@ -236,30 +121,5 @@ class TestPost:
               "['anime', 'show', 'movie']"
         assert ret == {
             "body": json.dumps({"message": msg}),
-            "statusCode": 400
-        }
-
-    @patch("api.watch_history_by_collection.watch_history_db.update_item")
-    def test_invalid_body(self, mocked_post):
-        mocked_post.return_value = True
-        event = copy.deepcopy(self.event)
-        event["body"] = "INVALID"
-
-        ret = handle(event, None)
-        assert ret == {"body": "Invalid post body", "statusCode": 400}
-
-    @patch("api.watch_history_by_collection.watch_history_db.update_item")
-    def test_invalid_body_schema(self, mocked_post):
-        mocked_post.return_value = True
-        event = copy.deepcopy(self.event)
-        event["body"] = '{"invalid": "val"}'
-
-        ret = handle(event, None)
-        body = {"message": "Invalid post schema",
-                "error": "Additional properties are not allowed "
-                         "('invalid' was unexpected)"
-                }
-        assert ret == {
-            "body": json.dumps(body),
             "statusCode": 400
         }
