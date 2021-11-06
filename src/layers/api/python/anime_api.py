437705d078044d683dc1445b515a68643d5cd3e3
fulder <misad90@gmail.com>
2021-11-06 12:29:42 +0100

    Remove episodes_by_collection_item lambda

--- src/layers/api/python/anime_api.py
+++ src/layers/api/python/anime_api.py
@@ -8,14 +8,6 @@ ANIME_API_URL = os.getenv("ANIME_API_URL")
 
 
 
-def get_episode(anime_id, episode_id, token):
-    res = requests.get(f"{ANIME_API_URL}/anime/{anime_id}/episodes/{episode_id}", headers={"Authorization": token})
-    if res.status_code != 200:
-        raise utils.HttpError("Invalid response in get_episode_by_api_id", res.status_code)
-
-    return res.json()
-
-
 def get_episode_by_api_id(anime_id, api_name, api_id, token):
     res = requests.get(f"{ANIME_API_URL}/anime/{anime_id}/episodes?api_name={api_name}&api_id={api_id}", headers={"Authorization": token})
     if res.status_code != 200:
