6da8f7a72c35c1ca19f9529694e0f6a00549a7d1
fulder <misad90@gmail.com>
2021-11-06 12:38:08 +0100

    Add jikan episodes generator and add get_episode func

--- src/layers/api/python/jikan.py
+++ src/layers/api/python/jikan.py
@@ -17,6 +17,12 @@ class JikanApi:
             raise utils.HttpError(res.status_code)
         return res.json()
 
+    def get_episode(self, anime_id, episode_id):
+        for eps in self._episodes_generator(anime_id):
+            for ep in eps:
+                if ep["episode_id"] == episode_id:
+                    return ep
+
     def get_episodes(self, anime_id, page=1):
         res = requests.get(
             f"{self.base_url}/anime/{anime_id}/episodes/{page}",
@@ -27,18 +33,20 @@ class JikanApi:
         return res.json()
 
     def get_episode_count(self, anime_id):
-        page = 1
-        ret = self.get_episodes(anime_id, page)
-        last_page = ret["episodes_last_page"]
         ep_count = 0
 
-        while page != last_page:
-            page += 1
-            ret = self.get_episodes(anime_id, page)
-            ep_count += len(ret["episodes"])
+        for eps in self._episodes_generator(anime_id):
+            ep_count += len(eps)
 
         return {
             "ep_count": ep_count,
         }
 
+    def _episodes_generator(self, anime_id):
+        ret = self.get_episodes(anime_id)
+        last_page = ret["episodes_last_page"]
+
+        for i in range(1, last_page+1):
+            ret = self.get_episodes(anime_id, i)
+            yield ret["episodes"]
 
