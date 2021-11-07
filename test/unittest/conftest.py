fae1a42899f81ea07ab9043c2ea6885cfed0a816
fulder <misad90@gmail.com>
2021-11-07 19:51:53 +0100

    Cleanup conftest

--- test/unittest/conftest.py
+++ test/unittest/conftest.py
@@ -9,44 +9,6 @@ from api.watch_histories import app
 os.environ["LOG_LEVEL"] = "DEBUG"
 
 
-@pytest.fixture(scope='function')
-def mocked_watch_history_db():
-    import watch_history_db
-
-    watch_history_db.table = MagicMock()
-    watch_history_db.client = MagicMock()
-
-    return watch_history_db
-
-
-@pytest.fixture(scope='function')
-def mocked_anime_api():
-    import anime_api
-
-    anime_api.ANIME_API_URL = "https://mocked"
-
-    return anime_api
-
-
-@pytest.fixture(scope='function')
-def mocked_episodes_db():
-    import episodes_db
-
-    episodes_db.table = MagicMock()
-    episodes_db.client = MagicMock()
-
-    return episodes_db
-
-
-@pytest.fixture(scope='function')
-def mocked_movie_api():
-    import movie_api
-
-    movie_api.MOVIE_API_URL = "https://mocked"
-
-    return movie_api
-
-
 @pytest.fixture(scope='session')
 def client():
     return TestClient(app)
