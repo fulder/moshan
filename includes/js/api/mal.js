b8a097764a044dfc944cd6757f55b851cde1d456
fulder <misad90@gmail.com>
2021-11-07 11:47:23 +0100

    Remove debug log

--- includes/js/api/mal.js
+++ includes/js/api/mal.js
@@ -89,7 +89,6 @@ class MalApi {
   getMoshanEpisode(episode) {
     let date = 'N/A';
     if (episode.aired !== null) {
-      console.debug(episode.aired);
       date = new Date(episode.aired).toISOString().split('T')[0];
     }
 
