8983ac9490c6b2970276e94df09c4a7127e68e85
fulder <misad90@gmail.com>
2021-11-13 18:19:25 +0100

    Add title, status and startDate for tmdb/tvdb items

--- includes/js/backlog.js
+++ includes/js/backlog.js
@@ -15,20 +15,21 @@ async function createTableRows() {
     const response = await watchHistoryApi.getWatchHistory('backlog_date');
     const items = response.data.items;
 
-    // const apiRequests = [];
-    // for (let i=0; i < items.length; i++) {
-    //   const apiName = items[i]['api_name'];
-    //   const apiId = items[i]['api_id'];
-    //   const api = getApiByName(apiName);
-    //
-    //   apiRequests.push(api.getItemById({ 'api_id': apiId }));
-    // }
-    //
-    // const responses = await Promise.all(apiRequests);
-    // const moshanItems = {};
-    // for (let i=0; i< responses.length; i++) {
-    //   moshanItems[`${responses[i].api_name}_${responses[i].id}`] = responses[i];
-    // }
+    const apiRequests = [];
+    for (let i=0; i < items.length; i++) {
+      const apiName = items[i]['api_name'];
+      const apiId = items[i]['api_id'];
+      if (apiName !== 'mal') {
+        const api = getApiByName(apiName);
+        apiRequests.push(api.getItemById({ 'api_id': apiId }));
+      }
+    }
+
+    const responses = await Promise.all(apiRequests);
+    const moshanItems = {};
+    for (let i=0; i< responses.length; i++) {
+      moshanItems[`${responses[i].api_name}_${responses[i].id}`] = responses[i];
+    }
 
     html = '';
     for (let i=0; i< items.length; i++) {
@@ -38,28 +39,36 @@ async function createTableRows() {
 }
 
 
-function createRow(watchHistoryItem) {
-  // moshanItem = moshanItems[`${watchHistoryItem.api_name}_${watchHistoryItem.api_id}`];
+function createRow(watchHistoryItem, moshanItems) {
+  moshanItem = moshanItems[`${watchHistoryItem.api_name}_${watchHistoryItem.api_id}`];
 
-  let rowClass = 'episodeRow';
-  // let rowClass = 'bg-secondary';
-  // if (moshanItem.status === 'Released' || moshanItem.status === 'Airing' || moshanItem.status === 'Ended' || moshanItem.status === 'Running') {
-  //     rowClass = 'episodeRow';
-  // }
-  // if (new Date(moshanItem.start_date) >= new Date()) {
-  //     rowClass = 'bg-secondary';
-  // }
+  let rowClass = 'bg-secondary';
+  let title = 'N/A';
+  let status = 'N/A';
+  let startDate = 'N/A';
+  if (moshanItem !== undefined) {
+    if (moshanItem.status === 'Released' || moshanItem.status === 'Airing' || moshanItem.status === 'Ended' || moshanItem.status === 'Running') {
+        rowClass = 'episodeRow';
+    }
+    if (new Date(moshanItem.start_date) >= new Date()) {
+        rowClass = 'bg-secondary';
+    }
+
+    title = moshanItem.title;
+    status = moshanItem.status;
+    startDate = moshanItem.start_date;
+  }
 
   const onClickAction = `window.location='item/index.html?api_name=${watchHistoryItem.api_name}&api_id=${watchHistoryItem.api_id}'`;
   return `
   <tr onclick="${onClickAction}" class=${rowClass}>
       <td>${watchHistoryItem.created_at}</td>
       <td>${watchHistoryItem.api_name}</td>
-
+      <td>${title}</td>
+      <td>${status}</td>
+      <td>${startDate}</td>
     </tr>
     `;
 
-    // <td>${moshanItem.title}</td>
-    // <td>${moshanItem.status}</td>
-    // <td>${moshanItem.start_date}</td>
+
 }
