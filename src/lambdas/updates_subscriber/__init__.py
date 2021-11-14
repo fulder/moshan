6111259f0c03b13f42f08abc6167c000e175ffe6
fulder <misad90@gmail.com>
2021-11-14 17:52:05 +0100

    Update cache in updates subscriber and add tmdb api

--- src/lambdas/updates_subscriber/__init__.py
+++ src/lambdas/updates_subscriber/__init__.py
@@ -1,20 +1,49 @@
 import json
 
 import jikan
+import tmdb
 import tvmaze
 import reviews_db
 
+tmdb_api = tmdb.TmdbApi()
 tvmaze_api = tvmaze.TvMazeApi()
 jikan_api = jikan.JikanApi()
 
 
 def handler(event, context):
     message = json.loads(event["Records"][0]["Sns"]["Message"])
+    api_name = message["api_name"]
+    api_id = message["api_id"]
 
-    if message["api_name"] == "tvmaze":
-        episodes_info = tvmaze_api.get_show_episodes_count(message["api_id"])
-    elif message["api_name"] == "mal":
-        episodes_info = jikan_api.get_episode_count(message["api_id"])
+    episodes_info = {}
+    if api_name == "tmdb":
+        api_item = tmdb_api.get_item(api_name)
+        api_cache = {
+            "title": api_item.get("title"),
+            "release_date": api_item.get("release_date"),
+            "status": api_item.get("status")
+        }
+    elif api_name == "tvmaze":
+        api_item = tvmaze_api.get_item(api_id)
+        episodes_info = tvmaze_api.get_show_episodes_count(api_id)
+        api_cache = {
+            "title": api_item.get("name"),
+            "release_date": api_item.get("premiered"),
+            "status": api_item.get("status"),
+            "ep_count": episodes_info.get("ep_count"),
+            "special_count": episodes_info.get("special_count"),
+        }
+    elif api_name == "mal":
+        api_item = jikan_api.get_item(api_id)
+        episodes_info = jikan_api.get_episode_count(api_id)
+        api_cache = {
+            "title": api_item.get("title"),
+            "release_date": api_item.get("aired", {}).get("from"),
+            "status": api_item.get("status"),
+            "ep_count": episodes_info.get("ep_count"),
+            "special_count": episodes_info.get("special_count"),
+        }
+
     else:
         raise Exception(f"Unexpected api_name: {message['api_name']}")
 
@@ -37,26 +66,27 @@ def handler(event, context):
         item = {
             **item,
             **count_info,
+            "api_cache": api_cache,
         }
 
         reviews_db.put_item(item)
 
 
 def _get_item_counts(episodes_info, watched_eps, watched_specials):
-    ep_count = episodes_info["ep_count"]
-    counts = {
-        "watched_eps": watched_eps,
-        "ep_count": ep_count,
-        "ep_progress": _get_progress(watched_eps, ep_count),
-    }
+    counts = {}
+    if "episodes_count" in episodes_info:
+        p = _get_progress(watched_eps, episodes_info["ep_count"])
+        counts = {
+            "watched_eps": watched_eps,
+            "ep_progress": p,
+        }
 
     if "special_count" in episodes_info:
-        special_count = episodes_info["special_count"]
+        p = _get_progress(watched_specials, episodes_info["special_count"])
         counts = {
             **counts,
             "watched_specials": watched_specials,
-            "special_count": special_count,
-            "special_progress": _get_progress(watched_specials, special_count),
+            "special_progress": p,
         }
 
     return counts
