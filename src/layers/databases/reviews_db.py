f6f6f3bba7fed3cec15054cf8be0db38b8798318
fulder <misad90@gmail.com>
2021-11-14 14:09:53 +0100

    Remove scan index forward property

--- src/layers/databases/reviews_db.py
+++ src/layers/databases/reviews_db.py
@@ -152,7 +152,6 @@ def _get_review(username, api_info, include_deleted=False):
 def get_all_items(username, sort=None, cursor=None):
     kwargs = {
         "KeyConditionExpression": Key("username").eq(username),
-        # "ScanIndexForward": False,
         "Limit": 50,
     }
     if sort is not None:
