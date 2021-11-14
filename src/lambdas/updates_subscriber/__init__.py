643b27e33a8eeabcced312eb9a3b4b12828ee65e
fulder <misad90@gmail.com>
2021-11-14 17:45:23 +0100

    Refactor updates subscriber lambda

--- src/lambdas/updates_subscriber/__init__.py
+++ src/lambdas/updates_subscriber/__init__.py
@@ -11,39 +11,60 @@ jikan_api = jikan.JikanApi()
 def handler(event, context):
     message = json.loads(event["Records"][0]["Sns"]["Message"])
 
+    if message["api_name"] == "tvmaze":
+        episodes_info = tvmaze_api.get_show_episodes_count(message["api_id"])
+    elif message["api_name"] == "mal":
+        episodes_info = jikan_api.get_episode_count(message["api_id"])
+    else:
+        raise Exception(f"Unexpected api_name: {message['api_name']}")
+
     items = reviews_db.get_items(
         message["api_name"],
         message["api_id"]
     )
 
-    if message["api_name"] == "tvmaze":
-        api_item = tvmaze_api.get_show_episodes_count(message["api_id"])
-    elif message["api_name"] == "mal":
-        api_item = jikan_api.get_episode_count(message["api_id"])
-    else:
-        raise Exception(f"Unexpected api_name: {message['api_name']}")
-
     for item in items:
         print(f"Updating item: {item}")
 
-        if "watched_eps" not in item:
-            item["watched_eps"] = 0
-        if "watched_specials" not in item:
-            item["watched_specials"] = 0
+        watched_eps = item.get("watched_eps", 0)
+        watched_specials = item.get("watched_specials", 0)
 
-        item["ep_count"] = api_item["ep_count"]
-        if item["ep_count"] == 0:
-            ep_progress = 0
-        else:
-            ep_progress = item["watched_eps"] / item["ep_count"]
-        item["ep_progress"] = round(ep_progress * 100, 2)
-
-        if "special_count" in api_item:
-            item["special_count"] = api_item["special_count"]
-            if item["special_count"] == 0:
-                special_progress = 0
-            else:
-                special_progress = item["watched_specials"] / item["special_count"]
-            item["special_progress"] = round(special_progress * 100, 2)
+        count_info = _get_item_counts(
+            episodes_info,
+            watched_eps,
+            watched_specials
+        )
+        item = {
+            **item,
+            **count_info,
+        }
 
         reviews_db.put_item(item)
+
+
+def _get_item_counts(episodes_info, watched_eps, watched_specials):
+    ep_count = episodes_info["ep_count"]
+    counts = {
+        "watched_eps": watched_eps,
+        "ep_count": ep_count,
+        "ep_progress": _get_progress(watched_eps, ep_count),
+    }
+
+    if "special_count" in episodes_info:
+        special_count = episodes_info["special_count"]
+        counts = {
+            **counts,
+            "watched_specials": watched_specials,
+            "special_count": special_count,
+            "special_progress": _get_progress(watched_specials, special_count),
+        }
+
+    return counts
+
+
+def _get_progress(watched, count):
+    progress = 0
+    if count != 0:
+        progress = watched / count
+
+    return round(progress * 100, 2)
