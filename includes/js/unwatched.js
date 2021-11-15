d615633be3d57f731827ca7ac7e85b6675bc4a24
fulder <misad90@gmail.com>
2021-11-15 23:17:52 +0100

    Simplify unwatched page

--- includes/js/unwatched.js
+++ includes/js/unwatched.js
@@ -1,15 +1,8 @@
-/* global WatchHistoryApi, accessToken, collectionNames */
+/* global WatchHistoryApi, accessToken */
 // const urlParams = new URLSearchParams(window.location.search);
 // const qParams = new QueryParams(urlParams);
 
 const watchHistoryApi = new WatchHistoryApi();
-
-// TODO: move to profile settings
-const apiNamesMapping = {
-  'movie': 'tmdb',
-  'show': 'tvmaze',
-  'anime': 'mal',
-};
 
 if (accessToken === null) {
   document.getElementById('logInAlert').className = 'alert alert-danger';
@@ -23,47 +16,33 @@ createUnwatchedItems();
 //
 // }
 
-async function createUnwatchedItems() {
-  const watchHistoryRequests = [];
-  for (let i = 0; i < collectionNames.length; i++) {
-    const collectionName = collectionNames[i];
-    document.getElementById(`unwatched-${collectionName}`).innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
+async function createUnwatchedItems(cursor='') {
+  document.getElementById('unwatched').innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
 
-    const req = watchHistoryApi.getWatchHistoryByCollection(collectionName, 'ep_progress', apiNamesMapping['show']);
-    watchHistoryRequests.push(req);
-  }
+  const response = await watchHistoryApi.getWatchHistory('ep_progress', cursor);
 
-  const responses = await Promise.all(watchHistoryRequests);
-
-  for (let i = 0; i < collectionNames.length; i++) {
-      const collectionName = collectionNames[i];
-      console.log(responses[i]);
-      createItems(responses[i].data.items, collectionName);
-  }
+  createItems(response.data.items, collectionName);
 }
 
-async function createItems(wathcHistoryItems, collectionName) {
+async function createItems(wathcHistoryItems) {
   let resultHTML = '';
   let res = true;
   let itemCreated = false;
 
-  const apiName = apiNamesMapping[collectionName];
-  const api = getApiByName(apiName);
-
   console.debug(`Length: ${wathcHistoryItems.length}`);
 
   for (let i = 0; i < wathcHistoryItems.length; i++) {
-    const moshanItem = wathcHistoryItems[i];
-    const apiItem = api.getMoshanItem(wathcHistoryItems[i][`${apiName}_data`]);
-    console.debug(moshanItem);
+    const item = watchHistoryItems[i];
+    const apiCache = item.api_cache;
+
     const itemHTML = `
-        <div id="poster-show-${apiItem.id}" class="col-4 col-md-2 poster">
-          <a href="/item?collection=${collectionName}&api_name=${apiName}&api_id=${apiItem.id}">
-            <img class="img-fluid" src="${apiItem.poster}" />
+        <div class="col-4 col-md-2 poster">
+          <a href="/item?api_name=${item.api_name}&api_id=${item.api_id}">
+            <img class="img-fluid" src="${apiCache.image_url}" />
             <div class="progress">
-              <div class="progress-bar" role="progressbar" style="width: ${moshanItem.ep_progress}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${moshanItem.ep_progress}%</div>
+              <div class="progress-bar" role="progressbar" style="width: ${item.ep_progress}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${item.ep_progress}%</div>
             </div>
-            <p class="text-truncate small">${apiItem.title}</p>
+            <p class="text-truncate small">${apiCache.title}</p>
           </a>
       </div>
     `;
@@ -80,5 +59,5 @@ async function createItems(wathcHistoryItems, collectionName) {
     document.getElementById('itemsLoadingAlert').className = 'alert alert-warning';
   }
 
-  document.getElementById(`unwatched-${collectionName}`).innerHTML = resultHTML;
+  document.getElementById('unwatched').innerHTML += resultHTML;
 }
