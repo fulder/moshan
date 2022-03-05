260826ae2f6f36642e75cf99d387f14c5c59a941
fulder <misad90@gmail.com>
2022-03-05 23:46:55 +0100

    Add scroll loading from backlog to history page

--- includes/js/history.js
+++ includes/js/history.js
@@ -3,6 +3,9 @@
 // const qParams = new QueryParams(urlParams);
 const watchHistoryApi = new WatchHistoryApi();
 
+let currentCursor = null;
+let loadingMore = false;
+
 if (accessToken === null) {
   document.getElementById('logInAlert').className = 'alert alert-danger';
 } else {
@@ -13,6 +16,7 @@ createHistory();
 
 async function createHistory(cursor='') {
   const response = await watchHistoryApi.getWatchHistory('latest_watch_date', cursor);
+  currentCursor = response.data.end_cursor;
 
   createItems(response.data.items);
 }
@@ -47,3 +51,21 @@ async function createItems(items) {
 
   document.getElementById('history').innerHTML += resultHTML;
 }
+
+async function loadMore() {
+  if (loadingMore || currentCursor === null) {
+    // Don't trigger load more than once and only if there are more items
+    return;
+  }
+  loadingMore = true;
+
+  await createHistory(currentCursor);
+
+  loadingMore = false;
+}
+
+document.addEventListener('scroll', function() {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      loadMore();
+  }
+});
